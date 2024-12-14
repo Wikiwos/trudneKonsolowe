364a44b83631b129312f3123a6d0c4ea611a891d
Wiktor <wiktor-wosiek@wp.pl>
2024-12-14 13:35:02 +0100

    Prognoza Pogody

--- Pogoda.kt
+++ Pogoda.kt
@@ -1,4 +1,41 @@
 package com.example.trudnekonsolowe
 
-class Pogoda {
+interface Weather {
+    fun pogoda(): String
+}
+
+class Sunny : Weather {
+    override fun pogoda(): String {
+        return "Słonecznie"
+    }
+}
+
+class Rainy : Weather {
+    override fun pogoda(): String {
+        return "Deszczowo"
+    }
+}
+
+class Cloudy : Weather {
+    override fun pogoda(): String {
+        return "Pochmurno"
+    }
+}
+
+class City(val name: String, val weather: Weather)
+
+
+fun main() {
+    val cities = listOf(
+        City("Warszawa", Sunny()),
+        City("Gdańsk", Rainy()),
+        City("Kraków", Cloudy())
+    )
+
+    println("Wybierz miasto " + " 1 - Warszawa " + " 2 - Gdańsk" + " 3 - Kraków: ")
+
+    val choice = readLine()?.toIntOrNull()
+
+    val selectedCity = cities[choice!! - 1]
+    println("Prognoza pogody dla ${selectedCity.name}: ${selectedCity.weather.pogoda()}")
 }
